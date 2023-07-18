445238b04ef9d319eb4f417456d7768fa0fb0ba1
Carlos Olivares Croc <54370581+olivarescroc@users.noreply.github.com>
2023-07-18 00:29:48 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,52 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+
+# Usuario a Favorito es uno a muchos pues un usuario puede tener varios favoritos pero cada favorito es de solo un usuario
+class User(Base):
+    __tablename__= 'user'
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String(250))
+    lastname = Column(String(250))    
+    username = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    favorite = relationship('Favorite', backref='user')
+
+
+#Favoritos a planeta y personaje son relaciones de muchos a uno pues muchos registros de favoritos pueden referirse al mismo planeta o personaje pero cada
+#registro de favoritos solo puede referirse a un planeta o personaje
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id =Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+
+
+class Planet(Base):
+    __tablename__= 'planet'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    population = Column(Integer, nullable = False)
+    main_resource = Column(String(250), nullable=False)
+    rotarion_period = Column(Integer, nullable = False)
+    favorite = relationship('Favorite', backref='planet')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    age = Column(Integer, nullable = False)
+    height = Column(Integer, nullable = False)
+    gender = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    favorite = relationship('Favorite', backref='character')
+
+
 
     def to_dict(self):
         return {}
